53a12db71099bb4cb0d144a425dead14a60e2e6b
whyzhuce <chen_xu112358@outlook.com>
2016-12-19 21:42:06 +0800

    Update xconparraison.py

--- xconparraison.py
+++ xconparraison.py
@@ -7,6 +7,7 @@
 # WARNING! All changes made in this file will be lost!
 from pandas import read_excel
 from PyQt4 import QtCore, QtGui
+Qt = QtCore.Qt
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -23,7 +24,7 @@ except AttributeError:
         return QtGui.QApplication.translate(context, text, disambig)
 
 
-def colnum_string(n):
+def column_string(n):
     div=n+1
     string=""
     while div>0:
@@ -65,10 +66,8 @@ class Ui_XConparraison(object):
         self.leftSelect = QtGui.QComboBox(self.centralwidget)
         self.leftSelect.setObjectName(_fromUtf8("leftSelect"))
         self.gridLayout.addWidget(self.leftSelect, 1, 0, 1, 1)
-        self.leftTbl = QtGui.QTableWidget(self.centralwidget)
+        self.leftTbl = QtGui.QTableView(self.centralwidget)
         self.leftTbl.setObjectName(_fromUtf8("leftTbl"))
-        self.leftTbl.setColumnCount(0)
-        self.leftTbl.setRowCount(0)
         self.gridLayout.addWidget(self.leftTbl, 2, 0, 1, 1)
         self.gridLayout_3.addLayout(self.gridLayout, 0, 0, 1, 1)
         self.gridLayout_2 = QtGui.QGridLayout()
@@ -76,10 +75,8 @@ class Ui_XConparraison(object):
         self.rightSelect = QtGui.QComboBox(self.centralwidget)
         self.rightSelect.setObjectName(_fromUtf8("rightSelect"))
         self.gridLayout_2.addWidget(self.rightSelect, 1, 0, 1, 1)
-        self.rightTbl = QtGui.QTableWidget(self.centralwidget)
+        self.rightTbl = QtGui.QTableView(self.centralwidget)
         self.rightTbl.setObjectName(_fromUtf8("rightTbl"))
-        self.rightTbl.setColumnCount(0)
-        self.rightTbl.setRowCount(0)
         self.gridLayout_2.addWidget(self.rightTbl, 2, 0, 1, 1)
         self.splitter_2 = QtGui.QSplitter(self.centralwidget)
         self.splitter_2.setOrientation(QtCore.Qt.Horizontal)
@@ -106,45 +103,103 @@ class Ui_XConparraison(object):
         XConparraison.setStatusBar(self.statusbar)
 
         self.retranslateUi(XConparraison)
-        QtCore.QObject.connect(self.leftInput, QtCore.SIGNAL(_fromUtf8("returnPressed()")), self.getLeftSheets)
-        QtCore.QObject.connect(self.rightInput, QtCore.SIGNAL(_fromUtf8("returnPressed()")), self.getRightSheets)
+        QtCore.QObject.connect(self.leftInput, QtCore.SIGNAL(_fromUtf8("returnPressed()")), self.getLeftWorkBook)
+        QtCore.QObject.connect(self.rightInput, QtCore.SIGNAL(_fromUtf8("returnPressed()")), self.getRightWorkBook)
         QtCore.QObject.connect(self.leftSelect, QtCore.SIGNAL(_fromUtf8("activated(QString)")), self.showLeftSheet)
         QtCore.QObject.connect(self.rightSelect, QtCore.SIGNAL(_fromUtf8("activated(QString)")), self.showRightSheet)
-        QtCore.QObject.connect(self.CmpValue, QtCore.SIGNAL(_fromUtf8("clicked()")), self.leftTbl.clearContents)
+        QtCore.QObject.connect(self.CmpValue, QtCore.SIGNAL(_fromUtf8("clicked()")), self.compareValue)
+        QtCore.QObject.connect(self.CmpFormula, QtCore.SIGNAL(_fromUtf8("clicked()")), self.compareFormula)
         QtCore.QMetaObject.connectSlotsByName(XConparraison)
 
-    def getLeftSheets(self):
-        self.getSheets(str(self.leftInput.text()), self.leftSelect)
+    def getLeftWorkBook(self):
+        self.left = self.getWorkBook(str(self.leftInput.text()))
+        self.leftSelect.clear()
+        self.leftSelect.addItems(self.left.keys())
 
-    def getRightSheets(self):
-        self.getSheets(str(self.rightInput.text()), self.rightSelect)
+    def getRightWorkBook(self):
+        self.right = self.getWorkBook(str(self.rightInput.text()))
+        self.rightSelect.clear()
+        self.rightSelect.addItems(self.right.keys())
 
-    def getSheets(self, input, select):
-        select.clear()
+    def getWorkBook(self, input):
         wb = read_excel(input, header=None, sheetname=None)
-        select.addItems(wb.keys())
+        return wb
 
     def showLeftSheet(self):
-        self.showSheet(str(self.leftInput.text()), self.leftSelect, self.leftTbl)
+        self.showSheet(self.left, str(self.leftSelect.currentText()), self.leftTbl)
 
     def showRightSheet(self):
-        self.showSheet(str(self.rightInput.text()), self.rightSelect, self.rightTbl)
+        self.showSheet(self.right, str(self.rightSelect.currentText()), self.rightTbl)
 
-    def showSheet(self, input, select, tbl):
-        df = read_excel(input, header=None, sheetname=str(select.currentText())).fillna("")
-        tbl.setColumnCount(len(df.columns))
-        tbl.setRowCount(len(df.index))
-        for j in range(len(df.columns)):
-            tbl.setHorizontalHeaderItem(j, QtGui.QTableWidgetItem(colnum_string(j)))
-            for i in range(len(df.index)):
-                tbl.setItem(i, j, QtGui.QTableWidgetItem(str(df.iloc[i,j])))
-        tbl.resizeColumnsToContents()
-        tbl.resizeRowsToContents()
+    def showSheet(self, wb, sh, tbl):
+        df = wb[sh].fillna("")
+        header = map(column_string, map(int, df.columns))
+        tbl.setModel(PandasModel(df, header))
+        tbl.setItemDelegate(CompareDelegate())
+
+    def compareValue(self):
+        dl = self.left[str(self.leftSelect.currentText())].fillna("")
+        dr = self.right[str(self.rightSelect.currentText())].fillna("")
+        mask = dl.eq(dr)
+        compare_delegate = CompareDelegate(mask=mask)
+        self.leftTbl.setItemDelegate(compare_delegate)
+        self.rightTbl.setItemDelegate(compare_delegate)
+
+    def compareFormula(self):
+        pass
 
     def retranslateUi(self, XConparraison):
         XConparraison.setWindowTitle(_translate("XConparraison", "XConparraison", None))
         self.CmpValue.setText(_translate("XConparraison", "Compare Value", None))
         self.CmpFormula.setText(_translate("XConparraison", "Compare Formula", None))
+
+
+class CompareDelegate(QtGui.QItemDelegate):
+    def __init__(self, parent=None, mask=None, *args):
+        QtGui.QItemDelegate.__init__(self, parent, *args)
+        self.mask = mask
+
+    def paint(self, painter, option, index):
+        painter.save()
+
+        # set background color
+        painter.setPen(QtGui.QPen(Qt.NoPen))
+        if option.state & (self.mask is not None):
+            if self.mask.iloc[index.row(), index.column()] == False:
+                painter.setBrush(QtGui.QBrush(Qt.red))
+        else:
+            painter.setBrush(QtGui.QBrush(Qt.white))
+        painter.drawRect(option.rect)
+
+        # # set text color
+        painter.setPen(QtGui.QPen(Qt.black))
+
+        painter.restore()
+        QtGui.QItemDelegate.paint(self, painter, option, index)
+
+
+class PandasModel(QtCore.QAbstractTableModel):
+    def __init__(self, data, header, parent=None):
+        QtCore.QAbstractTableModel.__init__(self, parent)
+        self._data = data
+        self.header = header
+
+    def rowCount(self, *args, **kwargs):
+        return len(self._data.values)
+
+    def columnCount(self, *args, **kwargs):
+        return self._data.columns.size
+
+    def data(self, index, role=Qt.DisplayRole):
+        if index.isValid:
+            if role == Qt.DisplayRole:
+                return QtCore.QVariant(self._data.values[index.row()][index.column()])
+        return QtCore.QVariant()
+
+    def headerData(self, i, orientation, role):
+        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
+            return QtCore.QVariant(self.header[i])
+        return QtCore.QAbstractTableModel.headerData(self, i, orientation, role)
 
 
 if __name__ == "__main__":
